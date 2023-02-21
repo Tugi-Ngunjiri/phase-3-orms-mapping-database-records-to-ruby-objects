9b8b3270cc2f1b001f914dc570d9af0a08142620
tugi <blueqwinn@gmail.com>
2023-02-21 21:10:42 +0300

    Completed my assigment

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,22 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+
+# First song
+Song.all.first
+
+# Last Song
+Song.all.last
+
+# Last.name
+Song.all.last.name
+
+# last.name.reverse
+Song.all.last.name.reverse
+
+Song.all
+
+#find_by_name
+Song.find_by_name("Hello")
+
+
